9e4762d73f486cef59d89072437a7c4e5f50dceb
Seven <sjj233788@alibaba-inc.com>
2020-05-23 01:19:16 +0800

    feat: two-sum

---
+++ Week_01/easy/two-sum.js
@@ -0,0 +1,19 @@
+// 两数之和
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const hashTable = {};
+
+    for (let i = 0 ; i < nums.length; i++) {
+        if(hashTable[target - nums[i]] > -1) {
+            return [hashTable[target - nums[i]], i];
+        } else {
+            hashTable[nums[i]] = i;
+        }
+    }
+
+    return [];
+};
