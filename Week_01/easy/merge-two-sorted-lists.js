c3c491d9e528fcc116323155a367878e7e775f7a
Seven <sjj233788@alibaba-inc.com>
2020-05-23 01:02:22 +0800

    feat: merge-two-sorted-lists

---
+++ Week_01/easy/merge-two-sorted-lists.js
@@ -0,0 +1,33 @@
+// 合并两个有序链表
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    let dummy = new ListNode(0);
+    let temp = dummy;
+
+    while(l1 && l2) {
+        if (l1.val < l2.val) {
+            temp.next = l1;
+            l1 = l1.next;
+        } else {
+            temp.next = l2;
+            l2 = l2.next;
+        }
+
+        temp = temp.next;
+    }
+
+    temp.next = l1 === null ? l2 : l1;
+
+    return dummy.next;
+}
