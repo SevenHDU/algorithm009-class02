26103c55ad4bd45f8ba03ef63e72175344d33b92
Seven <sjj233788@alibaba-inc.com>
2020-06-07 02:20:28 +0800

    feat: 全排列

---
+++ Week_03/medium/permutations.js
@@ -0,0 +1,25 @@
+// 全排列
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let n = nums.length;
+    let res = [];
+    let tmpPath = [];
+    let backtrack = (tmpPath) => {
+        if(tmpPath.length == n){
+            res.push(tmpPath);
+            return;
+        }
+        for(let i = 0;i < n;i++){
+            if(!tmpPath.includes(nums[i])){
+                tmpPath.push(nums[i]);
+                backtrack(tmpPath.slice());
+                tmpPath.pop();
+            }
+        }
+    }
+    backtrack(tmpPath);
+    return res;
+};
