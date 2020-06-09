bf551dca0c940d30607bc4f8ea5261386a5e060b
Seven <sjj233788@alibaba-inc.com>
2020-06-10 02:21:30 +0800

    feat: 券排列II

--- Week_03/medium/permutations-ii.js
+++ Week_03/medium/permutations-ii.js
@@ -4,5 +4,37 @@
  * @return {number[][]}
  */
 var permuteUnique = function(nums) {
+    let res = [];
+    const count = nums.length;
+    const used = {};
+    let path = [];
+    nums.sort((a, b) => a - b);
 
+    function backtrack(path , used) {
+        if (path.length === count) {
+            res.push(path.slice());
+            return ;
+        }
+
+        for (let i = 0; i < count; i++) {
+            if (!!used[i]) {
+                continue;
+            }
+
+            if (i > 0 && nums[i - 1] === nums[i] && !!used[i - 1] === false) {
+                continue;
+            }
+
+            used[i] = true;
+            path.push(nums[i]);
+            backtrack(path, used);
+
+            used[i] = false;
+            path.pop();
+        }
+    }
+
+    backtrack(path, used);
+
+    return res;
 };
